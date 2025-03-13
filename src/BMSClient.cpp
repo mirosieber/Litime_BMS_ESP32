81d7080ac8c1a8f6547b229c33bb219263ee9411
Miro Sieber <160486278+Msfjel383@users.noreply.github.com>
2025-03-13 15:19:55 +0100

    empty problem gelöst

--- src/BMSClient.cpp
+++ src/BMSClient.cpp
@@ -21,8 +21,7 @@ void BMSClient::init(const char* deviceAddress) {
 
 bool BMSClient::connect() {
     if(connected) return true;
-    // Korrektur 2: empty() → isEmpty()
-    if(deviceAddress.toString().empty()) return false;
+    if(deviceAddress.toString().length() == 0) return false;
 
     if(!BLEDevice::getInitialized()) {
         BLEDevice::init("ESP32-BMS-Client");
